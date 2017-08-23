6063b078a0f9f8f9941bf4bc69b16f3e1d4fcdd4
fengxin5571 <502251245@qq.com>
2017-08-23 10:24:10 +0800

    2017 8 23 map

--- mobile/control/area.php
+++ mobile/control/area.php
@@ -39,14 +39,39 @@ class areaControl extends mobileHomeControl{
     /*
      * 找营养初始化地图
      */
-    public function get_cityOp(){
+    public function init_mapOp(){
         $area=new ip_area();
         $ip=getIp()?getIp():"127.0.0.1";
         $city_name=$area->getcity($ip);
         if($city_name[city]=="LAN"){
             $city_name['city']="太原市";
         }
-        
+        $data["area"]=$city_name;
+        $model_store = Model('store');
+        //查询条件
+        $condition = array();
+        //所需字段
+        $fields = "*";
+        //排序方式
+        $order = "store_id desc";
+        $store_list = $model_store->where($condition)->order($order)->select();
+        $own_store_list = $store_list;
+        $simply_store_list = array();
+        foreach ($own_store_list as $key => $value) {
+            $simply_store_list[$key]['store_name'] = $own_store_list[$key]['store_name'];
+            $simply_store_list[$key]['store_address'] = $own_store_list[$key]['store_address'];
+            $simply_store_list[$key]['live_store_address'] = $own_store_list[$key]['live_store_address'];
+            $simply_store_list[$key]['live_store_tel'] = $own_store_list[$key]['live_store_tel'];
+            $simply_store_list[$key]['store_area_info'] = $own_store_list[$key]['area_info'];
+            $simply_store_list[$key]['store_avatar'] =$own_store_list[$key]['store_avatar'];
+            $simply_store_list[$key]['goods_count'] = $own_store_list[$key]['goods_count'];
+            $simply_store_list[$key]['store_avatar_url'] = UPLOAD_SITE_URL.'/'.ATTACH_COMMON.DS.C('default_store_avatar');
+            if($own_store_list[$key]['store_avatar']){
+                $simply_store_list[$key]['store_avatar_url'] = UPLOAD_SITE_URL.'/shop/store/'.$own_store_list[$key]['store_avatar'];
+            }
+        }
+        $data['store_list']=$simply_store_list;
+        output_data($data);
        
     } 
 
